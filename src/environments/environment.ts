100bfa1b7c2c9b41ea194becde9a5e0f5927f05b
MoneshMS <moneshsanju@gmail.com>
2022-01-16 12:08:43 +0530

    Added build for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  BACKEND_URL: 'https://find-events-kec.herokuapp.com/'
+  production: true,
+  BACKEND_URL: 'http://localhost:3000/'
 };
 
 /*
